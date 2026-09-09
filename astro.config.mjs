e43f56bed7eee66c74be2d6c1c1be142ba4566ab
Amine Cartable <amine@sajomtech.dev>
2026-09-09 19:54:36 +0000

    Deploy GH Pages + clean node_modules, fix 404 route

---
+++ astro.config.mjs
@@ -0,0 +1,7 @@
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+  site: 'https://sajomtech-commits.github.io',
+  base: '/cartable-Numerique-Amine',
+  build: { format: 'directory' },
+});
